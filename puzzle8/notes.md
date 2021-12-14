0488cc906a0fd0af6cb81f70af18ad06a728d344
Thibaut VERCUEIL <thibaut.vercueil@st.com>
2021-12-14 15:38:46 +0100

    Cleanup

--- puzzle8/notes.md
+++ puzzle8/notes.md
@@ -1,4 +1,4 @@
-0: 6 
+0: 6
 1: 2
 2: 5
 3: 5
@@ -9,14 +9,11 @@
 8: 7
 9: 6
 
-
-
-
 1 trouve le c/f
 
-1 et 7 -> trouve le A 
+1 et 7 -> trouve le A
 
-4 et 7 -> trouve le b/d 
+4 et 7 -> trouve le b/d
 
 trouve 9 (celui qui a 6 et qui a c/f b/d +A ) -> le G
 
@@ -25,4 +22,3 @@ trouve le 3 (celui qui a 5 et qui a c/f A et G ) -> le D et le B (l'autre)
 trouve le 5 (celui qui a 5 et qui A B D G) --> trouve F et C (l'autre)
 
 dans le 2 (celui qui reste a 5 chiffres ) --> trouver le E
-
--- puzzle9/p1/main.cpp
+++ puzzle9/p1/main.cpp
@@ -1,81 +1,70 @@
+#include <algorithm>
 #include <fstream>
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
-
-#define RESET   "\033[0m"
-#define BLACK   "\033[30m"      /* Black */
-#define RED     "\033[31m"      /* Red */
-#define GREEN   "\033[32m"      /* Green */
-#define YELLOW  "\033[33m"      /* Yellow */
-#define BLUE    "\033[34m"      /* Blue */
-#define MAGENTA "\033[35m"      /* Magenta */
-#define CYAN    "\033[36m"      /* Cyan */
-#define WHITE   "\033[37m"      /* White */
-
+#define RESET "\033[0m"
+#define BLACK "\033[30m"   /* Black */
+#define RED "\033[31m"     /* Red */
+#define GREEN "\033[32m"   /* Green */
+#define YELLOW "\033[33m"  /* Yellow */
+#define BLUE "\033[34m"    /* Blue */
+#define MAGENTA "\033[35m" /* Magenta */
+#define CYAN "\033[36m"    /* Cyan */
+#define WHITE "\033[37m"   /* White */
 
 int main() {
 
-  std::ifstream input_file("../input.txt");
+    std::ifstream input_file("../input.txt");
 
-  std::string line;
+    std::string line;
 
-  std::vector<std::vector<int>> height_map;
+    std::vector<std::vector<int>> height_map;
 
-  while(std::getline(input_file, line)) {
+    while (std::getline(input_file, line)) {
 
-    std::vector<int> row;
+        std::vector<int> row;
 
-    std::transform(line.begin(), line.end(), std::back_inserter(row),
-        [](const char c){return c - '0';});
+        std::transform(line.begin(), line.end(), std::back_inserter(row),
+                       [](const char c) { return c - '0'; });
 
-
-    height_map.push_back(row);
-  }
-
-  int total = 0;
-
-  for(int row = 0; row < height_map.size(); row++) {
-    for(int col = 0; col < height_map[0].size(); col++){
-      auto val = height_map[row][col];
-
-      int up = 10;
-      int down = 10; 
-      int left = 10; 
-      int right = 10; 
-
-      if( row > 0){
-        up = height_map[row-1][col];
-      }
-      if( row < height_map.size() -1 ){
-        down = height_map[row+1][col];
-      }
-      if( col > 0){
-        left = height_map[row][col-1];
-      }
-      if( col < height_map[0].size() -1 ){
-        right = height_map[row][col+1];
-      }
-
-
-      if( val < up && 
-          val < down &&
-          val < right &&
-          val < left){
-
-        total += 1+val;
-        std::cout << RED << val << RESET;
-      }
-      else {
-        std::cout << val;
-      }
-
+        height_map.push_back(row);
     }
-    std::cout<< std::endl;
-  }
 
+    int total = 0;
 
-  std::cout << "TOTAL " << total << std::endl;
+    for (int row = 0; row < height_map.size(); row++) {
+        for (int col = 0; col < height_map[0].size(); col++) {
+            auto val = height_map[row][col];
 
+            int up = 10;
+            int down = 10;
+            int left = 10;
+            int right = 10;
+
+            if (row > 0) {
+                up = height_map[row - 1][col];
+            }
+            if (row < height_map.size() - 1) {
+                down = height_map[row + 1][col];
+            }
+            if (col > 0) {
+                left = height_map[row][col - 1];
+            }
+            if (col < height_map[0].size() - 1) {
+                right = height_map[row][col + 1];
+            }
+
+            if (val < up && val < down && val < right && val < left) {
+
+                total += 1 + val;
+                std::cout << RED << val << RESET;
+            } else {
+                std::cout << val;
+            }
+        }
+        std::cout << std::endl;
+    }
+
+    std::cout << "TOTAL " << total << std::endl;
 }
